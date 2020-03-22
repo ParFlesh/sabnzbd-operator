734f4ea51435f633b280361b06eca2701f771619
parflesh <parflesh@gmail.com>
2020-03-22 01:35:05 -0500

    Add status updates;Merge config and additional volumes;Add deployment reconcile;update example cr;Add build template

--- pkg/controller/sabnzbd/sabnzbd_controller.go
+++ pkg/controller/sabnzbd/sabnzbd_controller.go
@@ -7,6 +7,9 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"reflect"
+	"strings"
+	"time"
 
 	sabnzbdv1alpha1 "github.com/parflesh/sabnzbd-operator/pkg/apis/sabnzbd/v1alpha1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -103,6 +106,11 @@ func (r *ReconcileSABnzbd) Reconcile(request reconcile.Request) (reconcile.Resul
 		if err != nil {
 			return reconcile.Result{}, err
 		}
+		newStatus.Phase = "Initializing"
+		newStatus.Reason = "Setting default spec settings"
+		newStatus.Deployments = r.checkDeploymentStatus(&appsv1.Deployment{})
+		newStatus.Image = instance.Spec.Image
+		_ = r.updateStatus(newStatus, instance)
 		return reconcile.Result{Requeue: true}, nil
 	}
 
@@ -110,15 +118,6 @@ func (r *ReconcileSABnzbd) Reconcile(request reconcile.Request) (reconcile.Resul
 	if err != nil {
 		return reconcile.Result{}, err
 	}*/
-
-	newStatus.Image = instance.Spec.Image
-	if newStatus.Image != instance.Status.Image {
-		instance.Status.Image = newStatus.Image
-		if err := r.client.Status().Update(context.TODO(), instance); err != nil {
-			return reconcile.Result{}, err
-		}
-		return reconcile.Result{Requeue: true}, nil
-	}
 
 	newDep, err := r.newDeployment(instance)
 	if err != nil {
@@ -131,9 +130,27 @@ func (r *ReconcileSABnzbd) Reconcile(request reconcile.Request) (reconcile.Resul
 		if err != nil {
 			return reconcile.Result{}, err
 		}
+		newStatus.Phase = "Initializing"
+		newStatus.Reason = "Created deployment"
+		_ = r.updateStatus(newStatus, instance)
 		return reconcile.Result{Requeue: true}, nil
 	} else if err != nil {
 		return reconcile.Result{}, err
+	}
+
+	newStatus.Deployments = r.checkDeploymentStatus(foundDep)
+	_ = r.updateStatus(newStatus, instance)
+
+	if err := r.reconcileDeployment(foundDep, newDep); err != nil {
+		reqLogger.Error(err, "Deployment.Namespace", foundDep.Namespace, "Deployment.Name", foundDep.Name)
+		if err := r.client.Update(context.TODO(), foundDep); err != nil {
+			return reconcile.Result{}, err
+		}
+		newStatus.Image = instance.Spec.Image
+		newStatus.Phase = "Updating"
+		newStatus.Reason = "Updating deployment"
+		_ = r.updateStatus(newStatus, instance)
+		return reconcile.Result{Requeue: true}, nil
 	}
 
 	newSvc, err := r.newService(instance)
@@ -147,12 +164,31 @@ func (r *ReconcileSABnzbd) Reconcile(request reconcile.Request) (reconcile.Resul
 		if err != nil {
 			return reconcile.Result{}, err
 		}
+		newStatus.Phase = "Initializing"
+		newStatus.Reason = "Created service"
+		_ = r.updateStatus(newStatus, instance)
 		return reconcile.Result{Requeue: true}, nil
 	} else if err != nil {
 		return reconcile.Result{}, err
 	}
 
-	return reconcile.Result{}, nil
+	if len(newStatus.Deployments[appsv1.DeploymentAvailable]) > 0 {
+		newStatus.Phase = string(appsv1.DeploymentAvailable)
+		newStatus.Reason = ""
+	} else if len(newStatus.Deployments[appsv1.DeploymentProgressing]) > 0 {
+		newStatus.Phase = string(appsv1.DeploymentProgressing)
+		newStatus.Reason = "Deployment progressing"
+	} else if len(newStatus.Deployments[appsv1.DeploymentReplicaFailure]) > 0 {
+		newStatus.Phase = string(appsv1.DeploymentReplicaFailure)
+		newStatus.Reason = "Deployment replica failure"
+	}
+	_ = r.updateStatus(newStatus, instance)
+
+	requeueTime, err := time.ParseDuration(instance.Spec.WatchFrequency)
+	if err != nil {
+		return reconcile.Result{RequeueAfter: time.Second * 60}, nil
+	}
+	return reconcile.Result{RequeueAfter: requeueTime}, nil
 }
 
 func (r *ReconcileSABnzbd) reconcileSpec(cr *sabnzbdv1alpha1.SABnzbd) error {
@@ -170,6 +206,16 @@ func (r *ReconcileSABnzbd) reconcileSpec(cr *sabnzbdv1alpha1.SABnzbd) error {
 func (r *ReconcileSABnzbd) newDeployment(cr *sabnzbdv1alpha1.SABnzbd) (*appsv1.Deployment, error) {
 	labels := r.labelsForCR(cr)
 
+	volumes, volumeMounts, err := r.parseVolumes(cr.Spec.Volumes)
+	if err != nil {
+		return &appsv1.Deployment{}, err
+	}
+
+	var imagePullSecrets []corev1.LocalObjectReference
+	for _, s := range cr.Spec.ImagePullSecrets {
+		imagePullSecrets = append(imagePullSecrets, corev1.LocalObjectReference{Name: s})
+	}
+
 	dep := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      cr.Name,
@@ -186,12 +232,7 @@ func (r *ReconcileSABnzbd) newDeployment(cr *sabnzbdv1alpha1.SABnzbd) (*appsv1.D
 					Labels: labels,
 				},
 				Spec: corev1.PodSpec{
-					Volumes: []corev1.Volume{
-						{
-							Name:         "config",
-							VolumeSource: cr.Spec.ConfigVolume,
-						},
-					},
+					Volumes: volumes,
 					Containers: []corev1.Container{
 						{
 							Name:  "sabnzbd",
@@ -203,13 +244,8 @@ func (r *ReconcileSABnzbd) newDeployment(cr *sabnzbdv1alpha1.SABnzbd) (*appsv1.D
 									Protocol:      corev1.ProtocolTCP,
 								},
 							},
-							Resources: corev1.ResourceRequirements{},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "config",
-									MountPath: "/config",
-								},
-							},
+							Resources:    corev1.ResourceRequirements{},
+							VolumeMounts: volumeMounts,
 							LivenessProbe: &corev1.Probe{
 								Handler: corev1.Handler{
 									HTTPGet: &corev1.HTTPGetAction{
@@ -241,7 +277,7 @@ func (r *ReconcileSABnzbd) newDeployment(cr *sabnzbdv1alpha1.SABnzbd) (*appsv1.D
 					},
 					RestartPolicy:     corev1.RestartPolicyAlways,
 					SecurityContext:   &corev1.PodSecurityContext{},
-					ImagePullSecrets:  cr.Spec.ImagePullSecrets,
+					ImagePullSecrets:  imagePullSecrets,
 					PriorityClassName: cr.Spec.PriorityClassName,
 				},
 			},
@@ -253,14 +289,18 @@ func (r *ReconcileSABnzbd) newDeployment(cr *sabnzbdv1alpha1.SABnzbd) (*appsv1.D
 	}
 
 	if cr.Spec.RunAsUser != int64(0) {
-		dep.Spec.Template.Spec.Containers[0].SecurityContext.RunAsUser = &cr.Spec.RunAsUser
+		dep.Spec.Template.Spec.SecurityContext.RunAsUser = &cr.Spec.RunAsUser
 	}
 
 	if cr.Spec.RunAsGroup != int64(0) {
-		dep.Spec.Template.Spec.Containers[0].SecurityContext.RunAsUser = &cr.Spec.RunAsUser
+		dep.Spec.Template.Spec.SecurityContext.RunAsUser = &cr.Spec.RunAsUser
 	}
 
-	err := controllerutil.SetControllerReference(cr, dep, r.scheme)
+	if cr.Spec.FSGroup != int64(0) {
+		dep.Spec.Template.Spec.SecurityContext.FSGroup = &cr.Spec.FSGroup
+	}
+
+	err = controllerutil.SetControllerReference(cr, dep, r.scheme)
 	if err != nil {
 		return dep, err
 	}
@@ -301,8 +341,122 @@ func (r *ReconcileSABnzbd) newService(cr *sabnzbdv1alpha1.SABnzbd) (*corev1.Serv
 	return dep, nil
 }
 
+func (r *ReconcileSABnzbd) reconcileDeployment(f *appsv1.Deployment, p *appsv1.Deployment) error {
+	if !reflect.DeepEqual(f.Spec.Template.Spec.Volumes, p.Spec.Template.Spec.Volumes) || !reflect.DeepEqual(f.Spec.Template.Spec.Containers[0].VolumeMounts, p.Spec.Template.Spec.Containers[0].VolumeMounts) {
+		f.Spec.Template.Spec.Volumes = p.Spec.Template.Spec.Volumes
+		f.Spec.Template.Spec.Containers[0].VolumeMounts = p.Spec.Template.Spec.Containers[0].VolumeMounts
+		return fmt.Errorf("deployment volumes/volumemounts mismatch")
+	}
+
+	if f.Spec.Template.Spec.PriorityClassName != p.Spec.Template.Spec.PriorityClassName {
+		f.Spec.Template.Spec.PriorityClassName = p.Spec.Template.Spec.PriorityClassName
+		return fmt.Errorf("priority class name mismatch")
+	}
+
+	if !reflect.DeepEqual(f.Spec.Template.Spec.SecurityContext.RunAsUser, p.Spec.Template.Spec.SecurityContext.RunAsUser) {
+		f.Spec.Template.Spec.SecurityContext.RunAsUser = p.Spec.Template.Spec.SecurityContext.RunAsUser
+		return fmt.Errorf("user mismatch")
+	}
+
+	if !reflect.DeepEqual(f.Spec.Template.Spec.SecurityContext.RunAsGroup, p.Spec.Template.Spec.SecurityContext.RunAsGroup) {
+		f.Spec.Template.Spec.SecurityContext.RunAsGroup = p.Spec.Template.Spec.SecurityContext.RunAsGroup
+		return fmt.Errorf("group mismatch")
+	}
+
+	if !reflect.DeepEqual(f.Spec.Template.Spec.SecurityContext.FSGroup, p.Spec.Template.Spec.SecurityContext.FSGroup) {
+		f.Spec.Template.Spec.SecurityContext.FSGroup = p.Spec.Template.Spec.SecurityContext.FSGroup
+		return fmt.Errorf("filesystem group mismatch")
+	}
+
+	if f.Spec.Template.Spec.Containers[0].Image != p.Spec.Template.Spec.Containers[0].Image {
+		f.Spec.Template.Spec.Containers[0].Image = p.Spec.Template.Spec.Containers[0].Image
+		return fmt.Errorf("image mismatch")
+	}
+
+	if !reflect.DeepEqual(f.Spec.Template.Spec.ImagePullSecrets, p.Spec.Template.Spec.ImagePullSecrets) {
+		f.Spec.Template.Spec.ImagePullSecrets = p.Spec.Template.Spec.ImagePullSecrets
+		return fmt.Errorf("image pull secrets mismatch")
+	}
+
+	if !reflect.DeepEqual(f.Labels, p.Labels) || !reflect.DeepEqual(f.Spec.Template.Labels, p.Spec.Template.Labels) || !reflect.DeepEqual(f.Spec.Selector.MatchLabels, p.Spec.Selector.MatchLabels) {
+		f.Labels = p.Labels
+		f.Spec.Template.Labels = p.Spec.Template.Labels
+		f.Spec.Selector.MatchLabels = p.Spec.Selector.MatchLabels
+		return fmt.Errorf("labels mismatch")
+	}
+
+	if *f.Spec.Replicas != *p.Spec.Replicas {
+		f.Spec.Replicas = p.Spec.Replicas
+		return fmt.Errorf("replicas mismatch")
+	}
+	return nil
+}
+
 func (r *ReconcileSABnzbd) labelsForCR(cr *sabnzbdv1alpha1.SABnzbd) map[string]string {
 	return map[string]string{
 		"sabnzbd": cr.Name,
 	}
+}
+
+func (r *ReconcileSABnzbd) parseVolumes(cr []sabnzbdv1alpha1.SABnzbdSpecVolume) ([]corev1.Volume, []corev1.VolumeMount, error) {
+	var volumes []corev1.Volume
+	var volumeMounts []corev1.VolumeMount
+
+	for _, vol := range cr {
+		volumes = append(volumes, corev1.Volume{
+			Name: vol.Name,
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: vol.Claim,
+				},
+			},
+		})
+		volumeMounts = append(volumeMounts, corev1.VolumeMount{
+			Name:      vol.Name,
+			MountPath: vol.MountPath,
+			SubPath:   vol.SubPath,
+		})
+	}
+
+	return volumes, volumeMounts, nil
+}
+
+func (r *ReconcileSABnzbd) updateStatus(status sabnzbdv1alpha1.SABnzbdStatus, cr *sabnzbdv1alpha1.SABnzbd) error {
+	if !reflect.DeepEqual(status, cr.Status) {
+		cr.Status = status
+		if err := r.client.Status().Update(context.TODO(), cr); err != nil {
+			reqLogger := log.WithValues("Request.Namespace", cr.Namespace, "Request.Name", cr.Name)
+			reqLogger.Error(err, "Status", status)
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *ReconcileSABnzbd) checkDeploymentStatus(dep *appsv1.Deployment) map[appsv1.DeploymentConditionType][]string {
+	output := map[appsv1.DeploymentConditionType][]string{
+		appsv1.DeploymentAvailable:      {},
+		appsv1.DeploymentReplicaFailure: {},
+		appsv1.DeploymentProgressing:    {},
+	}
+
+	for _, c := range dep.Status.Conditions {
+		if c.Status == corev1.ConditionTrue {
+			if c.Type == appsv1.DeploymentProgressing && strings.Contains(c.Message, "has successfully progressed") {
+				continue
+			}
+			output[c.Type] = append(output[c.Type], dep.Name)
+		}
+	}
+
+	if len(output[appsv1.DeploymentProgressing]) > 0 {
+		output[appsv1.DeploymentAvailable] = []string{}
+	}
+	if len(output[appsv1.DeploymentReplicaFailure]) > 0 {
+		output[appsv1.DeploymentAvailable] = []string{}
+		output[appsv1.DeploymentProgressing] = []string{}
+
+	}
+
+	return output
 }
